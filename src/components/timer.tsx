f976180bb64e7e82658e247376a7781811038aea
MaaMachado <maamachadox@gmail.com>
2023-10-10 13:56:15 -0300

    Código finalizado

--- src/components/timer.tsx
+++ src/components/timer.tsx
@@ -1,13 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import React, {useState} from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, SafeAreaView, Button } from 'react-native';
 
-export default function App() {
+export default function Timer() {
+
+  const [hora, setHora] = useState<string>('');
+
+  useEffect(() => {
+    const atual = setInterval(() => {
+      const agora = new Date();
+      const horas = agora.getHours().toString().padStart(2, '0');
+      const minutos = agora.getMinutes().toString().padStart(2, '0');
+      const segundos = agora.getSeconds().toString().padStart(2, '0');
+
+      setHora(`${horas}:${minutos}:${segundos}`);
+    }, 1000);
+
+    return () => clearInterval(atual);
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.box}>
+        <Text style={styles.text}>{hora}</Text>
+      </View>
+
+      <View style={styles.btn}>
+        <Button title="🔁Atualizar Hora" onPress={() => atual()} />
+      </View>
+    </SafeAreaView> 
   );
 }
 
@@ -18,4 +38,26 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  box:{
+    backgroundColor: '#19CFFC',
+    borderColor: '#1BB3EF',
+    padding: 15,
+    borderRadius: 10,
+    elevation: 4,
+  },
+  text: {
+    fontSize: 32,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    color: '#004',
+  },
+  btn: {
+    marginTop: 15,
+    backgroundColor: '#00A6ED',
+    color: '#fff',
+    fontWeight: 'bold',
+    textAlign: 'center',
+    fontSize: 18,
+    padding: 10,
+  }
 });
